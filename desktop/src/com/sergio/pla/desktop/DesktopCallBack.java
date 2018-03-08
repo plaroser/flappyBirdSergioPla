610e48ab10445841641bd6fa21bb67c730abd739
Sergio <https://plaroser@bitbucket.org/plaroser/pruebaandroid.git>
2018-03-08 16:36:36 +0100

    Funcionalidad en Desktop reparada

---
+++ desktop/src/com/sergio/pla/desktop/DesktopCallBack.java
@@ -0,0 +1,19 @@
+package com.sergio.pla.desktop;
+
+import com.sergio.pla.utils.GameCallBack;
+
+/**
+ * Created by Sergio on 3/8/2018.
+ */
+
+public class DesktopCallBack implements GameCallBack {
+    @Override
+    public void callBack() {
+
+    }
+
+    @Override
+    public boolean isSound() {
+        return true;
+    }
+}
--- desktop/src/com/sergio/pla/desktop/DesktopLauncher.java
+++ desktop/src/com/sergio/pla/desktop/DesktopLauncher.java
@@ -3,13 +3,24 @@ package com.sergio.pla.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.sergio.pla.game.SergioPlaGame;
+import com.sergio.pla.utils.GameCallBack;
 
-public class DesktopLauncher {
+public class DesktopLauncher implements GameCallBack {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Game Sergio";
 		config.width = 272*2;
 		config.height = 408*2;
-		new LwjglApplication(new SergioPlaGame(), config);
+		new LwjglApplication(new SergioPlaGame(new DesktopLauncher()), config);
+	}
+
+	@Override
+	public void callBack() {
+
+	}
+
+	@Override
+	public boolean isSound() {
+		return false;
 	}
 }
