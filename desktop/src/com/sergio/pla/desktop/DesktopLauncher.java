f51e75ccfeb195052b31b62f066f74add570f2d5
MSI-SOBREMESA <git@bitbucket.org:plaroser/pruebaandroid.git>
2018-03-08 16:52:56 +0100

    Error en DesktopLauncher Reparado

--- desktop/src/com/sergio/pla/desktop/DesktopLauncher.java
+++ desktop/src/com/sergio/pla/desktop/DesktopLauncher.java
@@ -5,22 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.sergio.pla.game.SergioPlaGame;
 import com.sergio.pla.utils.GameCallBack;
 
-public class DesktopLauncher implements GameCallBack {
+public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Game Sergio";
 		config.width = 272*2;
 		config.height = 408*2;
-		new LwjglApplication(new SergioPlaGame(new DesktopLauncher()), config);
-	}
-
-	@Override
-	public void callBack() {
-
-	}
-
-	@Override
-	public boolean isSound() {
-		return false;
+		new LwjglApplication(new SergioPlaGame(new DesktopCallBack()), config);
 	}
 }
