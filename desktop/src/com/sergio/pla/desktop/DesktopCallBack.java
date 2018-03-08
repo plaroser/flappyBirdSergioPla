2eeb1b1a7f99e8f2610ded44e2d9d354f6521c72
MSI-SOBREMESA <git@bitbucket.org:plaroser/pruebaandroid.git>
2018-03-08 19:16:13 +0100

    Desktop launcher reparado

--- desktop/src/com/sergio/pla/desktop/DesktopCallBack.java
+++ desktop/src/com/sergio/pla/desktop/DesktopCallBack.java
@@ -16,4 +16,14 @@ public class DesktopCallBack implements GameCallBack {
     public boolean isSound() {
         return true;
     }
+
+    @Override
+    public void setHighScore(int score) {
+
+    }
+
+    @Override
+    public int getHighScore() {
+        return 0;
+    }
 }
